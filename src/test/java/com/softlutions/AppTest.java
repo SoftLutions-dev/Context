8c5a9094fb72b41f91bdab8399811dfdde456055
dgallardo <douglasgallardoz11@gmail.com>
2022-01-10 00:47:14 -0500

    Se corrige pipeline

--- src/test/java/com/softlutions/AppTest.java
+++ src/test/java/com/softlutions/AppTest.java
@@ -9,12 +9,10 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue( false );
     }
 }
